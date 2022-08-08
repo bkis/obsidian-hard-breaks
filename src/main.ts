f7268b72ece6b9cc5bc64f76606d27a88185a9f9
bkis <kiss@mailbox.org>
2022-08-08 16:33:49 +0200

    Remove unused code

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,6 @@ import remarkGfm from 'remark-gfm';
 
 const selectNodes = selectAll;
 
-const range = (start: number, stop: number, step: number = 1) =>
-  Array(Math.ceil((stop - start) / step)).fill(start).map((x, y) => x + y * step);
-
 interface HardBreaksPluginSettings {
   hardBreakFormat: '  ' | '\\';
 }
